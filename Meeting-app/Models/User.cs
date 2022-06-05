ead79caee81fb099cb96bc6cf4731000f6573701
PauliusCiganas <89936529+PauliusCiganas@users.noreply.github.com>
2022-06-05 22:08:03 +0300

    Initial commit

---
+++ Meeting-app/Models/User.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.Json;
+
+namespace Meeting_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public static List<User> UserList = new List<User>();
+
+        public static User CurrentUser = null;
+
+        const string FILE_NAME = "Users.json";
+
+
+
+
+        public static void Register()
+        {
+            Console.Clear();
+            bool exit = false;
+            while (!exit)
+            {
+                if (File.Exists(FILE_NAME))
+                {
+                    var data = File.ReadAllText(FILE_NAME);
+                    var objectData = JsonSerializer.Deserialize<List<User>>(data);
+                    UserList = objectData;
+                }
+
+                Console.WriteLine("Please select a username");
+                string username = Console.ReadLine();
+                Console.WriteLine("Please select a password");
+                string password = Console.ReadLine();
+                
+                var user = UserList.Where(x => x.Name == username).FirstOrDefault();
+                if (user != null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("User already exists");
+                        continue;
+                    }
+
+                var newUser = new User() { Name = username, Password = password};
+
+                CurrentUser = newUser;
+                UserList.Add(newUser);
+                var jsonString = JsonSerializer.Serialize(UserList);
+                File.WriteAllText(FILE_NAME, jsonString);
+                
+                Console.Clear();
+                exit = true;
+            }
+        }
+
+
+        public static void Login()
+        {
+            Console.Clear();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Please enter your username");
+                string username = Console.ReadLine();
+                Console.WriteLine("Please enter your password");
+                string password = Console.ReadLine();
+
+                var user = UserList.Where(x => x.Name == username).FirstOrDefault();
+
+                if (user == null)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Register if you want to find friends!");
+                    continue;
+                }
+                if (user.Password == password && user.Name == username)
+                {
+                    Console.Clear();
+                    CurrentUser = user;
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Password is not correct");
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Meeting-app/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Meeting_app;
+using Meeting_app.Models;
+
+bool exit = false;
+int selection;
+
+string[] loginVariants = new string[]
+{
+    "Register",
+    "Login"
+};
+
+string[] menuVariants = new string[]
+{
+    "Create a new meeting",
+    "Delete an existing meeting",
+    "Add a person to the meeting",
+    "Remove person from a meeting",
+    "List of all created meetings",
+    "exit"
+};
+
+selection = Utility.getStringFromArray(loginVariants, "please select : ");
+
+switch (selection)
+{
+    case 0: User.Register();
+        break;
+    case 1: User.Login();
+        break;
+}
+
+
+selection = Utility.getStringFromArray(menuVariants, "please select");
+while (!exit)
+{
+    switch (selection)
+    {
+        case 0: Console.WriteLine("create meeting");
+            break;
+        case 1: Console.WriteLine("delete meeting");
+            break;
+        case 2: Console.WriteLine("add person");
+            break;
+        case 3: Console.WriteLine("remove person");
+            break;
+        case 4: Console.WriteLine("list all meetings");
+            break;
+        default: exit = true; break;
+    }
+    break;
+}
